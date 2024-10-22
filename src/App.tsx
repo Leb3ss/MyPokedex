6153bbf0911691db20a4b68ccd4b1e08be9867ac
STEPHEN BACHER <bacher.stephen@gmail.com>
2024-10-22 11:43:52 +0200

    Firstcommit pokedex

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import MyTitle from "./MyTitle";
+function App() {
+  return <MyTitle />;
+}
+export default App;
---
+++ src/MyTitle.tsx
@@ -0,0 +1,10 @@
+function MyTitle() {
+  return (
+    <section>
+      <h1>Hello React World</h1>
+      <p>Tu tombe bien mon loulou, j'ai remonté ma pendule !</p>
+      <p>Jim CARREY, 1994</p>
+    </section>
+  );
+}
+export default MyTitle;
